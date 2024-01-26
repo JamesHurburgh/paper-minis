8a360b40e7ab16b355f088b20fd8d694d6d631dd
James Hurburgh <JamesHurburgh@gmail.com>
2024-01-26 13:49:20 +1000

    chore: Added title

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar flat class="d-print-none">
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+      Paper Minis
     </v-app-bar-title>
   </v-app-bar>
 </template>
