0305f0b5bde5d3690f7b8deec0f3d5e372d3b94d
James Hurburgh <JamesHurburgh@gmail.com>
2024-01-28 00:16:45 +1000

    doc: Added footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <PaperMiniMaker />
+  <v-footer app>
+      <v-spacer></v-spacer>
+      <small>
+        <a href="https://github.com/JamesHurburgh/paper-minis">GitHub</a>
+        | <a href="https://github.com/JamesHurburgh/paper-minis/issues/new">Report issue</a>
+        | © {{ new Date().getFullYear() }} —
+        <strong>
+          Made by <a href="https://x.com/JamesHurburgh">James Hurburgh</a>
+        </strong>
+      </small>
+      <v-spacer></v-spacer>
+    </v-footer>
 </template>
 
 <script lang="ts" setup>
